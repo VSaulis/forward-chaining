732781a5cddc4787b5a13d74e0f63ed1b43852b0
Vytautas Saulis <vsaulis@trackforce.com>
2018-05-24 19:37:38 +0300

    Add project files.

---
+++ ForwardChaining/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ForwardChaining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
